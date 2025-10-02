75726959c007fc24380cb1f4917eaa6452236616
taiyebaafrin <taiyebasadia753@gmail.com>
2025-10-02 21:56:41 +0600

    sslcommerce added

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from product.views import ProductViewSet, CategoryViewSet, ReviewViewSet, ProductImageViewSet
-from order.views import CartViewSet, CartItemViewSet, OrderViewset
+from order.views import CartViewSet, CartItemViewSet, OrderViewset, intiate_payment
 from rest_framework_nested import routers
+
 
 router = routers.DefaultRouter()
 router.register('products', ProductViewSet, basename='products')
@@ -26,4 +27,5 @@ urlpatterns = [
     path('', include(cart_router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path("payment/initiate/", intiate_payment, name="intiate-payment")
 ]
