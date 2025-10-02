5a4b2d309ec0e73b3f2ee167b4a1ccfb6030f917
taiyebaafrin <taiyebasadia753@gmail.com>
2025-10-03 05:44:24 +0600

    up

--- order/views.py
+++ order/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from order import serializers as orderSz
@@ -11,6 +11,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from sslcommerz_lib import SSLCOMMERZ 
+from django.conf import settings as main_settings
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
@@ -104,7 +106,7 @@ def intiate_payment(request):
     post_body['total_amount'] = amount
     post_body['currency'] = "BDT"
     post_body['tran_id'] = f"trx_{order_id}"
-    post_body['success_url'] = "http://localhost:5173/dashboard/payment/success/"
+    post_body['success_url'] = f"{main_settings.BACKEND_URL}/api/v1/payment/success/"
     post_body['fail_url'] = "http://localhost:5173/dashboard/payment/fail/"
     post_body['cancel_url'] = "http://localhost:5173/dashboard/orders/"
     post_body['emi_option'] = 0
@@ -114,17 +116,32 @@ def intiate_payment(request):
     post_body['cus_add1'] = user.address
     post_body['cus_city'] = "Dhaka"
     post_body['cus_country'] = "Bangladesh"
-    post_body['shipping_method'] = "NO"
+    post_body['shipping_method'] = "No"
     post_body['multi_card_name'] = ""
     post_body['num_of_item'] = num_items
     post_body['product_name'] = "Ecommerce Products"
     post_body['product_category'] = "General"
     post_body['product_profile'] = "general"
 
-
     response = sslcz.createSession(post_body) # API response
-    print(response)
 
     if response.get("status") =='SUCCESS':
         return Response({"payment_url": response['GatewayPageURL']})
     return Response({"error": "Payment initiation failed"}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def payment_success(request):
+    order_id = "transaction_id",request.data.get("tran_id").split('_')[1]
+    order = Order.objects.get(id=order_id)
+    order.status = "Ready to Ship"
+    order.save()
+    return HttpResponseRedirect(f"{main_settings.FRONTEND_DOMAIN}/dashboard/orders/")
+
+@api_view(['POST'])
+def payment_cancel(request):
+    return HttpResponseRedirect(f"{main_settings.FRONTEND_DOMAIN}/dashboard/orders/")
+
+
+@api_view(['POST'])
+def payment_fail(request):
+    return HttpResponseRedirect(f"{main_settings.FRONTEND_DOMAIN}/dashboard/orders/")
